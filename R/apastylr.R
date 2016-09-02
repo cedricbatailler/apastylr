56e948a99f96c101400f063909579820b90fca78
Cedric <cedric.batailler@hotmail.com>
2016-09-02 22:38:31 +0200

    Added statistic parameter, cleaned the code

--- R/apastylr.R
+++ R/apastylr.R
@@ -7,7 +7,7 @@
 
 #' @importFrom broom tidy
 
-apastylr <- function(model) {
+apastylr <- function(model, statistics = "T") {
   if (!requireNamespace("broom", quietly = TRUE)) {
     stop("broom needed for this function to work. Please install it.",
          call. = FALSE)
@@ -18,21 +18,51 @@ apastylr <- function(model) {
          call. = FALSE)
   }
   
+  # Cleaning model
+  
   summary <- broom::tidy(model)
   
-  summary$APA <- paste0("t(",
-                        model$df.residual,
-                        ") = ",
-                        format(abs(round(summary$statistic, 2)), nsmall = 2),
-                        ", p ",
-                        ifelse(summary$p.value < .001,
-                               "< .001",
-                               paste0("= ",
-                                      sub(".", "",
-                                          format(round(summary$p.value, 3), nsmall = 3)))))
+  # Creating temprary table for treatment
+  
+  table <- data.frame(term = summary$term)
+  
+  # Get F and T statistics and degree of freedom
+  
+  table$t <- format(abs(round(summary$statistic, 2)), nsmall = 2)
+  table$f <- format(abs(round(summary$statistic^2, 2)), nsmall = 2)
+  table$df <- model$df.residual
+  
+  # Get pvalue as APA formatted text and as number
+  
+  table$pvalue_nb <- summary$p.value
+  table$pvalue_txt <- sub(".", "", format(round(summary$p.value, 3), nsmall = 3))
+  
+  # Creating text for each coefficient according to statistic
+  # parameter, either t or F
+  
+  if(statistics == "T") {
+    table$stattext <- paste0("t(", table$df, ") = ", table$t)
+  } else if(statistics == "F") {
+    table$stattext <- paste0("F(1, ", table$df, ") = ", table$f)
+  } else {
+    stop("statistics parameter must be either \"T\" or \"F\".",
+         call. = FALSE);
+  }
+  
+  # Creating p-value text for each coefficient
+  
+  table$pvaluetext <- ifelse(table$pvalue_nb < .001,
+                   "p < .001",
+                   table$pvalue_txt)
 
-  summary <- data.frame(Term = summary$term,
-                        APA  = summary$APA)
+  table$APA <- paste0(table$stattext, ", ", table$pvaluetext)
+
+  # Creating APA formatted summary
+  
+  summary <- data.frame(Term = table$term,
+                        APA  = table$APA)
+  
+  # Returning APA formated summary
   
   return(summary)
 }
